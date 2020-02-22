bfd9029fcdb3b157c9c7a36de19e1612dfd09556
manthanmkulakarni <31768411+manthanmkulakarni@users.noreply.github.com>
2020-02-22 18:31:27 +0000

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,19 +1,48 @@
 #include<iostream>
 #include<bits/stdc++.h>
+#include<cstring>
 
 using namespace std;
 
 class BSearch{
 public:
-	int ar[100],n;
-	BSearch(int i){
-		n=i;
+	int ar[100],n,count;
+	char fname[100];
+	fstream fptr;
+	char ename[100][100];
+	char address[100][100];
+	char gen[100][1];
+	char depname[100][100];
+	char ssn[100][100];
+	char salary[100][100];
+	BSearch(char *name){
+		n=0;
+		strcpy(fname,name);
 	}
 	void getData(){
-		cout<<"Enter the data\n";
-		for(int i=0;i<n;i++)
-			cin>>ar[i];
+		fptr.open(fname);
+		char data[1000],*datat,*temp,t[100];
+		int i=0;
+
+	
+		while(fptr>>data){
+		datat=data;
+		strcpy(ssn[i],strtok_r(datat,",",&datat));ar[i]=atoi(ssn[i]);
+		strcpy(ename[i],strtok_r(datat,",",&datat));
+		strcpy(address[i],strtok_r(datat,",",&datat));
+		strcpy(gen[i],strtok_r(datat,",",&datat));
+		strcpy(depname[i],strtok_r(datat,",",&datat));
+		strcpy(salary[i],strtok_r(datat,",",&datat));
+		i+=1;
+		}
+		fptr.close();
+		n=count=i;
+		for(int j=0;j<count;j++){
+			cout<<"\nEmployee "<<j<<endl;
+			cout<<ssn[j]<<"\n"<<ename[j]<<"\n"<<address[j]<<"\n"<<gen[j]<<"\n"<<depname[j]<<"\n"<<salary[j]<<"\n\n";
+		}
 	}
+
 	void printData(){
 		for(int i=0;i<n;i++)cout<<ar[i]<<" ";
 
@@ -21,6 +50,16 @@ public:
 	}
 	void sort(){
 		std::sort(ar,ar+n);
+	}
+
+	void printRec(int key){
+		for(int i=0;i<count;i++){
+			if(atoi(ssn[i])==key){
+				cout<<ssn[i]<<"\n"<<ename[i]<<"\n"<<address[i]<<"\n"<<gen[i]<<"\n"<<depname[i]<<"\n"<<salary[i]<<"\n\n";
+				break;		
+			}
+		}
+		
 	}
 	void search(){
 		int key,l=0,u=n-1,mid;
@@ -30,6 +69,7 @@ public:
 			mid=(l+u)/2;
 			if(ar[mid]==key){
 				cout<<"Key found at : "<<mid<<endl;
+				printRec(ar[mid]);
 				return;
 			}
 			else if(ar[mid]>key){
@@ -50,11 +90,8 @@ public:
 
 int main(){
 
-int n;
-cout<<"Enter the size of array\n";
-cin>>n;
 
-BSearch b(n);
+BSearch b("bsearchrecord.csv");
 
 b.getData();
 b.printData();
